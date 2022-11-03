1ef2ad8baefcaef07b5975b67bb77c232aa9ad06
francis gonzales <fgonzales.private@icloud.com>
2022-11-03 09:29:12 +0800

    fix verification, added toda

--- app/src/main/java/com/todaliveryph/todaliverymarketdeliveryapp/activities/RegisterRiderActivity.java
+++ app/src/main/java/com/todaliveryph/todaliverymarketdeliveryapp/activities/RegisterRiderActivity.java
@@ -13,16 +13,19 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.telephony.SmsManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResult;
@@ -48,8 +51,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -57,6 +63,7 @@ import com.todaliveryph.todaliverymarketdeliveryapp.R;
 
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class RegisterRiderActivity extends AppCompatActivity /*implements LocationListener*/ {
@@ -66,14 +73,17 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
     private EditText fullnameET,shopnameET, phoneET,deliveryfeeET, addressET, emailET, passwordET, cpasswordET,otpET;
     private ScrollView scrollView;
     private LinearLayout verificationLayout;
+    private Spinner spinner;
     private Button registerBTN, verifyBTN;
 
     //permission constants
    // public static final int LOCATION_REQUEST_CODE = 100;
     public static final int CAMERA_REQUEST_CODE = 200;
+
     public static final int STORAGE_REQUEST_CODE = 300;
     public static final int IMAGE_PICK_GALLERY_CODE = 400;
     public static final int IMAGE_PICK_CAMERA_CODE = 500;
+    public static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 600;
 
 
     //permission arrays
@@ -94,6 +104,7 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
     private ProgressDialog progressDialog;
 
     private String verificationId;
+    int otp = new Random().nextInt(900000) + 100000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +126,15 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
         registerBTN = findViewById(R.id.registerBTN);
         scrollView = findViewById(R.id.scrollView);
         verificationLayout = findViewById(R.id.otpVerificationView);
+        spinner = findViewById(R.id.spinner);
+
+
+
+
+        String[] items = new String[]{"STA.CRUZ TODA", "SANTIAGO,LAUCPAO TODA", "SAN PABLO 1st TODA", "SAN PABLO 2nd TODA",
+        "DELA PAZ TODA", "SAN PEDRO SAUG, SANPEDRO PALCARANGAN TODA", "SAN JOSE GUMI TODA","BALANTACAN TODA"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinner.setAdapter(adapter);
 
 
         //inside permissions array
@@ -147,15 +167,44 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
         });
 
         verifyBTN.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                progressDialog.setMessage("Verifying");
+                progressDialog.show();
                 //validating if the OTP text field is empty or not.
                 if (TextUtils.isEmpty(otpET.getText().toString())){
                     //if the OTP text field is empty display a message to user to enter OTP
                     Toast.makeText(RegisterRiderActivity.this, "Please enter OTP", Toast.LENGTH_SHORT).show();
                 }else{
-                    //if OTP field is not empty calling method to verify the OTP.
-                    verifyCode(otpET.getText().toString());
+                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+                    ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                            .addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    for(DataSnapshot ds: snapshot.getChildren()){
+                                        String otpFromDb =""+ds.child("otp").getValue();
+                                        if (otpFromDb.equals(otpET.getText().toString())){
+
+                                            Toast.makeText(RegisterRiderActivity.this,"You are verified", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(RegisterRiderActivity.this, MainRiderActivity.class));
+                                            finish();
+                                            progressDialog.dismiss();
+                                        }else {
+                                            Toast.makeText(RegisterRiderActivity.this,"Otp is not match", Toast.LENGTH_SHORT).show();
+                                            progressDialog.dismiss();
+                                        }
+
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(RegisterRiderActivity.this,error.getMessage(), Toast.LENGTH_SHORT).show();
+                                    progressDialog.dismiss();
+                                }
+                            });
+
 
                 }
 
@@ -202,6 +251,7 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
             Toast.makeText(this, "Enter Phone Number ...", Toast.LENGTH_SHORT).show();
             return;
         }
+
         if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
             Toast.makeText(this, "Invalid Email Address Format ...", Toast.LENGTH_SHORT).show();
             return;
@@ -242,109 +292,22 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
                 });
     }
 
-    private void sendVerificationCode(String number) {
-
-        PhoneAuthOptions options =
-                PhoneAuthOptions.newBuilder(firebaseAuth)
-                        .setPhoneNumber(phoneNumber)       // Phone number to verify
-                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                        .setActivity(this)                 // Activity (for callback binding)
-                        .setCallbacks(mCallBack)          // OnVerificationStateChangedCallbacks
-                        .build();
-        PhoneAuthProvider.verifyPhoneNumber(options);
-    }
-
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
-            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
-        //below method is used when OTP is sent from Firebase
-        @Override
-        public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-            super.onCodeSent(s, forceResendingToken);
-            //when we recieve the OTP it contains a unique id wich we are storing in our string which we have already created.
-            scrollView.setVisibility(View.GONE);
-            verificationLayout.setVisibility(View.VISIBLE);
-            verificationId = s;
-        }
-
-        //this method is called when user recieve OTP from Firebase.
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-            //below line is used for getting OTP code which is sent in phone auth credentials.
-            final String code = phoneAuthCredential.getSmsCode();
-            //checking if the code is null or not.
-            if (code != null) {
-                //if the code is not null then we are setting that code to our OTP edittext field.
-                otpET.setText(code);
-                //after setting this code to OTP edittext field we are calling our verifycode method.
-                verifyCode(code);
+    private void sendVerificationCode() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
 
             }
-
         }
-
-        //thid method is called when firebase doesnot sends our OTP code due to any error or issue.
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-            //displaying error message with firebase exception.
-            Toast.makeText(RegisterRiderActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
-            Log.d("error", e.getMessage());
-        }
-    };
-
-
-
-    private void verifyCode(String code) {
-        //below line is used for getting getting credentials from our verification id and code.
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
-        //after getting credential we are calling sign in method.
-
-       signInWithCredential(credential);
 
     }
 
-
-    private void signInWithCredential(PhoneAuthCredential credential) {
-        //inside this method we are checking if the code entered is correct or not.
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            HashMap<String, Object> hashMap = new HashMap<>();
-                            hashMap.put("uid",""+firebaseAuth.getUid());
-                            hashMap.put("isNumberVerified","true");
-                            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
-                            ref.child(firebaseAuth.getUid()).updateChildren(hashMap)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void unused) {
-                                            // db updated
-                                            Intent i =new Intent(RegisterRiderActivity.this,LoginActivity.class);
-                                            startActivity(i);
-                                            finish();
-                                        }
-                                    }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            //db failed to update
-                                            progressDialog.dismiss();
-                                            startActivity(new Intent(RegisterRiderActivity.this, MainRiderActivity.class));
-                                            finish();
-                                            Toast.makeText(RegisterRiderActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                                        }
-                                    });
-
-
-
-
-                        } else {
-                            //if the code is not correct then we are displaying an error message to the user.
-                            Toast.makeText(RegisterRiderActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-    }
 
     private void saveFirebaseData() {
         progressDialog.setMessage("Uploading your information to our database...");
@@ -360,7 +323,9 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
             hashMap.put("email",""+email);
             hashMap.put("name",""+fullName);
             hashMap.put("phone",""+phoneNumber);
+            hashMap.put("otp",otp);
             hashMap.put("address",""+address);
+            hashMap.put("Toda",spinner.getSelectedItem());
             // hashMap.put("latitude",""+latitude);
             // hashMap.put("longitude",""+longitude);
             hashMap.put("timestamp",""+timestamp);
@@ -378,10 +343,12 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
                         @Override
                         public void onSuccess(Void unused) {
                             // db updated
-
+                            sendVerificationCode();
                             Toast.makeText(RegisterRiderActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                            sendVerificationCode(phoneET.getText().toString());
+
                             progressDialog.dismiss();
+                            scrollView.setVisibility(View.GONE);
+                            verificationLayout.setVisibility(View.VISIBLE);
 //                            startActivity(new Intent(RegisterRiderActivity.this, MainRiderActivity.class));
 //                            finish();
 
@@ -424,7 +391,9 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
                                 hashMap.put("email",""+email);
                                 hashMap.put("name",""+fullName);
                                 hashMap.put("phone",""+phoneNumber);
+                                hashMap.put("otp",otp);
                                 hashMap.put("address",""+address);
+                                hashMap.put("Toda",spinner.getSelectedItem());
                                 // hashMap.put("latitude",""+latitude);
                                 // hashMap.put("longitude",""+longitude);
                                 hashMap.put("timestamp",""+timestamp);
@@ -442,10 +411,11 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
                                             @Override
                                             public void onSuccess(Void unused) {
                                                 // db updated
-
+                                                sendVerificationCode();
                                                 Toast.makeText(RegisterRiderActivity.this, "Success", Toast.LENGTH_SHORT).show();
-                                                sendVerificationCode(phoneET.getText().toString());
                                                 progressDialog.dismiss();
+                                                scrollView.setVisibility(View.GONE);
+                                                verificationLayout.setVisibility(View.VISIBLE);
 
                                             }
                                         }).addOnFailureListener(new OnFailureListener() {
@@ -627,6 +597,24 @@ public class RegisterRiderActivity extends AppCompatActivity /*implements Locati
             }
             break;
 */
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SmsManager smsManager = SmsManager.getDefault();
+
+                    smsManager.sendTextMessage(phoneET.getText().toString(),null,"Otp for verification is: "+otp,null,null);
+//                    smsManager.sendTextMessage(phoneNo, null, message, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS sent.",
+                            Toast.LENGTH_LONG).show();
+
+
+                } else {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS faild, please try again.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+            break;
             case CAMERA_REQUEST_CODE:{
                 if (grantResults.length > 0) {
 
